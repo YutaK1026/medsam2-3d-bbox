e700e4008c0ecbfe0c988d060c6d689df4ba6dce
YutaK1026 <inheritans1026@gmail.com>
2024-10-16 18:54:39 +0900

    refactor: flakeなどを追加

--- 3d/main.py
+++ 3d/main.py
@@ -1,17 +1,16 @@
-import torch
-from sam2.build_sam import build_sam2_video_predictor
-import numpy as np
-import cv2
-import supervision as sv
-import matplotlib.pyplot as plt
-import os
-from PIL import Image
 import json
+import os
+
+import matplotlib.pyplot as plt
+import numpy as np
+from PIL import Image
+from sam2.build_sam import build_sam2_video_predictor
 
 CHECKPOINT = "../model/3d/latest_epoch.pth"
 CONFIG = "sam2_hiera_s.yaml"
 VIDEO_FRAMES_DIRECTORY_PATH = "../video_data/data"
 sam2_model = build_sam2_video_predictor(CONFIG, CHECKPOINT)
+
 
 def show_mask(mask, ax, obj_id=None, random_color=False):
     if random_color:
@@ -26,16 +25,35 @@ def show_mask(mask, ax, obj_id=None, random_color=False):
 
 
 def show_points(coords, labels, ax, marker_size=200):
-    pos_points = coords[labels==1]
-    neg_points = coords[labels==0]
-    ax.scatter(pos_points[:, 0], pos_points[:, 1], color='green', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
-    ax.scatter(neg_points[:, 0], neg_points[:, 1], color='red', marker='*', s=marker_size, edgecolor='white', linewidth=1.25)
+    pos_points = coords[labels == 1]
+    neg_points = coords[labels == 0]
+    ax.scatter(
+        pos_points[:, 0],
+        pos_points[:, 1],
+        color="green",
+        marker="*",
+        s=marker_size,
+        edgecolor="white",
+        linewidth=1.25,
+    )
+    ax.scatter(
+        neg_points[:, 0],
+        neg_points[:, 1],
+        color="red",
+        marker="*",
+        s=marker_size,
+        edgecolor="white",
+        linewidth=1.25,
+    )
 
 
 def show_box(box, ax):
     x0, y0 = box[0], box[1]
     w, h = box[2] - box[0], box[3] - box[1]
-    ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
+    ax.add_patch(
+        plt.Rectangle((x0, y0), w, h, edgecolor="green", facecolor=(0, 0, 0, 0), lw=2)
+    )
+
 
 def get_bbox(mask):
     """
@@ -45,17 +63,23 @@ def get_bbox(mask):
     大体円とかそんな形になっているはず
     これをすべて内包するbboxを生成したい -> x, yそれぞれの最大最小を出力すればよいのではないか
     """
-    mask_list = np.where(mask == True)
+    mask_list = np.where(mask)  # 元はmask == Trueだった．flakeに怒られてこうした
     x_max = np.amax(mask_list[1])
     x_min = np.amin(mask_list[1])
     y_max = np.amax(mask_list[2])
     y_min = np.amin(mask_list[2])
     box = np.array([x_min, y_min, x_max, y_max], dtype=np.float64).tolist()
     return box
-    
 
-def segment_with_bbox(frame_names: list[str], start_index: int, end_index: int, obj_id: int, box: np.ndarray):
-    inference_state = sam2_model.init_state(video_path = VIDEO_FRAMES_DIRECTORY_PATH)
+
+def segment_with_bbox(
+    frame_names: list[str],
+    start_index: int,
+    end_index: int,
+    obj_id: int,
+    box: np.ndarray,
+):
+    inference_state = sam2_model.init_state(video_path=VIDEO_FRAMES_DIRECTORY_PATH)
     sam2_model.reset_state(inference_state)
 
     _, out_obj_ids, out_mask_logits = sam2_model.add_new_points_or_box(
@@ -63,11 +87,13 @@ def segment_with_bbox(frame_names: list[str], start_index: int, end_index: int, 
         frame_idx=start_index,
         obj_id=obj_id,
         box=box,
-    ) 
+    )
     # bboxを付与
 
     video_segments = {}
-    for out_frame_idx, out_obj_ids, out_mask_logits in sam2_model.propagate_in_video(inference_state):
+    for out_frame_idx, out_obj_ids, out_mask_logits in sam2_model.propagate_in_video(
+        inference_state
+    ):
         video_segments[out_frame_idx] = {
             out_obj_id: (out_mask_logits[i] > 0.0).cpu().numpy()
             for i, out_obj_id in enumerate(out_obj_ids)
@@ -79,7 +105,11 @@ def segment_with_bbox(frame_names: list[str], start_index: int, end_index: int, 
     for out_frame_idx in range(start_index, end_index, vis_frame_stride):
         plt.figure(figsize=(6, 4))
         plt.title(f"frame {out_frame_idx}")
-        plt.imshow(Image.open(os.path.join(VIDEO_FRAMES_DIRECTORY_PATH, frame_names[out_frame_idx])))
+        plt.imshow(
+            Image.open(
+                os.path.join(VIDEO_FRAMES_DIRECTORY_PATH, frame_names[out_frame_idx])
+            )
+        )
         for out_obj_id, out_mask in video_segments[out_frame_idx].items():
             if True in out_mask:
                 dict = {}
@@ -87,21 +117,31 @@ def segment_with_bbox(frame_names: list[str], start_index: int, end_index: int, 
                 dict["box"] = get_bbox(out_mask)
                 new_list.append(dict)
             show_mask(out_mask, plt.gca(), obj_id=out_obj_id)
-        plt.savefig(f'./datas/data_010/image_{out_frame_idx}.png')
+        plt.savefig(f"./datas/data_010/image_{out_frame_idx}.png")
     json.dump(new_list, dict_json)
+
 
 dict_json = open("datas/data.json", "w")
 
 frame_names = [
-        p for p in os.listdir(VIDEO_FRAMES_DIRECTORY_PATH)
-        if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG"]
-    ]
+    p
+    for p in os.listdir(VIDEO_FRAMES_DIRECTORY_PATH)
+    if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG"]
+]
 frame_names.sort(key=lambda p: int(os.path.splitext(p)[0]))
 box = np.array([161, 279, 204, 324], dtype=np.float32)
 
-segment_with_bbox(frame_names=frame_names, start_index=114, end_index=len(frame_names), obj_id=4, box=box)
+segment_with_bbox(
+    frame_names=frame_names,
+    start_index=114,
+    end_index=len(frame_names),
+    obj_id=4,
+    box=box,
+)
 
 # bboxラベルが付与されたものより上のsegmentationを行う
 frame_names = frame_names[0:114]
 frame_names.reverse()
-segment_with_bbox(frame_names=frame_names, start_index=0, end_index=114, obj_id=4, box=box)
+segment_with_bbox(
+    frame_names=frame_names, start_index=0, end_index=114, obj_id=4, box=box
+)
