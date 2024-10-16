58bda9763fda446de85003351af6372a48ac843d
YutaK1026 <inheritans1026@gmail.com>
2024-10-16 12:38:03 +0900

    add: 逆順にソートした後にセグメンテーションできるように修正

--- 3d/main.py
+++ 3d/main.py
@@ -10,8 +10,6 @@ from PIL import Image
 CHECKPOINT = "../model/3d/latest_epoch.pth"
 CONFIG = "sam2_hiera_s.yaml"
 VIDEO_FRAMES_DIRECTORY_PATH = "../video_data/data"
-
-
 sam2_model = build_sam2_video_predictor(CONFIG, CHECKPOINT)
 
 def show_mask(mask, ax, obj_id=None, random_color=False):
@@ -38,58 +36,54 @@ def show_box(box, ax):
     w, h = box[2] - box[0], box[3] - box[1]
     ax.add_patch(plt.Rectangle((x0, y0), w, h, edgecolor='green', facecolor=(0, 0, 0, 0), lw=2))
 
+
+def segment_with_bbox(frame_names: list[str], start_index: int, end_index: int, obj_id: int, box: np.ndarray):
+    inference_state = sam2_model.init_state(video_path = VIDEO_FRAMES_DIRECTORY_PATH)
+    sam2_model.reset_state(inference_state)
+
+    _, out_obj_ids, out_mask_logits = sam2_model.add_new_points_or_box(
+        inference_state=inference_state,
+        frame_idx=start_index,
+        obj_id=obj_id,
+        box=box,
+    ) # bboxを付与している．
+
+    # show the results on the current (interacted) frame
+    plt.figure(figsize=(9, 6))
+    plt.title(f"frame { start_index}")
+    plt.imshow(Image.open(os.path.join(VIDEO_FRAMES_DIRECTORY_PATH, frame_names[ start_index])))
+    show_box(box, plt.gca())
+    show_mask((out_mask_logits[0] > 0.0).cpu().numpy(), plt.gca(), obj_id=out_obj_ids[0])
+
+    # run propagation throughout the video and collect the results in a dict
+    video_segments = {}  # video_segments contains the per-frame segmentation results
+    for out_frame_idx, out_obj_ids, out_mask_logits in sam2_model.propagate_in_video(inference_state):
+        video_segments[out_frame_idx] = {
+            out_obj_id: (out_mask_logits[i] > 0.0).cpu().numpy()
+            for i, out_obj_id in enumerate(out_obj_ids)
+        }
+
+    # render the segmentation results every few frames
+    vis_frame_stride = 1
+    plt.close("all")
+    for out_frame_idx in range(start_index, end_index, vis_frame_stride):
+        plt.figure(figsize=(6, 4))
+        plt.title(f"frame {out_frame_idx}")
+        plt.imshow(Image.open(os.path.join(VIDEO_FRAMES_DIRECTORY_PATH, frame_names[out_frame_idx])))
+        for out_obj_id, out_mask in video_segments[out_frame_idx].items():
+            show_mask(out_mask, plt.gca(), obj_id=out_obj_id)
+        plt.savefig(f'./datas/data_010/image_{out_frame_idx}.png')
+
+
 frame_names = [
-    p for p in os.listdir(VIDEO_FRAMES_DIRECTORY_PATH)
-    if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG"]
-]
+        p for p in os.listdir(VIDEO_FRAMES_DIRECTORY_PATH)
+        if os.path.splitext(p)[-1] in [".jpg", ".jpeg", ".JPG", ".JPEG"]
+    ]
 frame_names.sort(key=lambda p: int(os.path.splitext(p)[0]))
-
-inference_state = sam2_model.init_state(video_path = VIDEO_FRAMES_DIRECTORY_PATH)
-sam2_model.reset_state(inference_state)
-
-ann_frame_idx = 114  # the frame index we interact with
-ann_obj_id = 6  # give a unique id to each object we interact with (it can be any integers)
-
-# Let's add a box at (x_min, y_min, x_max, y_max) = (300, 0, 500, 400) to get started
 box = np.array([161, 279, 204, 324], dtype=np.float32)
-_, out_obj_ids, out_mask_logits = sam2_model.add_new_points_or_box(
-    inference_state=inference_state,
-    frame_idx=ann_frame_idx,
-    obj_id=ann_obj_id,
-    box=box,
-)
 
-# show the results on the current (interacted) frame
-plt.figure(figsize=(9, 6))
-plt.title(f"frame {ann_frame_idx}")
-plt.imshow(Image.open(os.path.join(VIDEO_FRAMES_DIRECTORY_PATH, frame_names[ann_frame_idx])))
-show_box(box, plt.gca())
-show_mask((out_mask_logits[0] > 0.0).cpu().numpy(), plt.gca(), obj_id=out_obj_ids[0])
-plt.savefig(f'waai.png')
+segment_with_bbox(frame_names=frame_names, start_index=114, end_index=len(frame_names), obj_id=4, box=box)
 
-# run propagation throughout the video and collect the results in a dict
-video_segments = {}  # video_segments contains the per-frame segmentation results
-for out_frame_idx, out_obj_ids, out_mask_logits in sam2_model.propagate_in_video(inference_state):
-    video_segments[out_frame_idx] = {
-        out_obj_id: (out_mask_logits[i] > 0.0).cpu().numpy()
-        for i, out_obj_id in enumerate(out_obj_ids)
-    }
-
-# render the segmentation results every few frames
-vis_frame_stride = 1
-plt.close("all")
-for out_frame_idx in range(114, len(frame_names), vis_frame_stride):
-    plt.figure(figsize=(6, 4))
-    plt.title(f"frame {out_frame_idx}")
-    plt.imshow(Image.open(os.path.join(VIDEO_FRAMES_DIRECTORY_PATH, frame_names[out_frame_idx])))
-    for out_obj_id, out_mask in video_segments[out_frame_idx].items():
-        show_mask(out_mask, plt.gca(), obj_id=out_obj_id)
-    plt.savefig(f'./data_010/image_{out_frame_idx}.png')
-
-for out_frame_idx in range(114, 0, -vis_frame_stride):
-    plt.figure(figsize=(6, 4))
-    plt.title(f"frame {out_frame_idx}")
-    plt.imshow(Image.open(os.path.join(VIDEO_FRAMES_DIRECTORY_PATH, frame_names[out_frame_idx])))
-    for out_obj_id, out_mask in video_segments[out_frame_idx].items():
-        show_mask(out_mask, plt.gca(), obj_id=out_obj_id)
-    plt.savefig(f'./data_010/image_{out_frame_idx}.png')
+frame_names = frame_names[0:114]
+frame_names.reverse()
+segment_with_bbox(frame_names=frame_names, start_index=0, end_index=114, obj_id=4, box=box)
